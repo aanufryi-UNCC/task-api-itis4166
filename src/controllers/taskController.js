d40b0a99a2494be9ae3705a9fd4085cbc6d29557
Anufry1enak <aanufryi@uncc.edu>
2025-10-27 15:08:47 -0400

    updated changes

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -10,3 +10,12 @@ export async function createTask(req, res, next) {
   const task = await taskService.createTask({ title, completed });
   res.status(201).json(task);
 }
+
+export async function getTaskById(req, res, next) {
+  const { id } = req.params;
+  const task = await taskService.getTaskById(id);
+  if (!task) {
+    return res.status(404).json({ error: 'Task not found' });
+  }
+  res.status(200).json(task);
+}
